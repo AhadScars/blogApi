a43ba1d52ed7a4bc4e4a344f7e2e9e61c7674ef6
abdulahad007-ui <AhadScars>
2025-06-13 16:23:19 +0530

    add basic CRUD operations and integrate security

---
+++ src/main/java/com/example/BlogApi/service/UserService.java
@@ -0,0 +1,45 @@
+package com.example.BlogApi.service;
+
+import com.example.BlogApi.entity.UserEntity;
+import com.example.BlogApi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserEntity> getAll() {
+        return userRepository.findAll();
+    }
+
+    public UserEntity saveUser(UserEntity userEntity) {
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+        return userRepository.save(userEntity);
+    }
+
+    public void deleteAll() {
+        userRepository.deleteAll();
+    }
+
+    public UserEntity findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<UserEntity> findById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteById(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
