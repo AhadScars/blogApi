1b36a7e4fe156d72c9d94cbf67cc53d5f652671c
abdulahad007-ui <AhadScars>
2025-06-13 14:56:09 +0530

    Create New Blog Project

---
+++ src/main/java/com/example/BlogApi/BlogApiApplication.java
@@ -0,0 +1,20 @@
+package com.example.BlogApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiApplication.class, args);
+	}
+	@GetMapping("/greet")
+	public String greet(){
+		return "hello";
+	}
+
+}
